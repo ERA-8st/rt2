0655d6a891a9e7b73af1b8064045688bec0c9b2f
era <hera123230@gmail.com>
2020-07-29 17:39:48 +0900

    fix production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,8 +72,8 @@ Rails.application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['app178746235@heroku.com'],
+    :password       => ENV['xbklpc9p5198'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
